5efb5a3db257fe05e7eb6c45eaff54d20957c51c
jeiden83 <jeiden83@gmail.com>
2025-03-20 20:29:12 -0400

    Sasu

---
+++ commands/chat/meme/sasu.js
@@ -0,0 +1,12 @@
+async function run(message, args){
+
+    return `No Sasu, im the biggest fan`;
+}
+run.description = 
+{
+    'header' : 'Beat this Sasu',
+    'body' : undefined,
+    'usage' : undefined
+}
+
+module.exports = { run }
