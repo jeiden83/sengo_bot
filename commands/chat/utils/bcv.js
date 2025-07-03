2328264aad2b2df11aeb3481bb70aba0e1c183b4
jeiden83 <jeiden83@gmail.com>
2025-07-03 08:47:56 -0400

    Mas bonito el bcv

--- commands/chat/utils/bcv.js
+++ commands/chat/utils/bcv.js
@@ -3,14 +3,20 @@ const axios = require('axios');
 async function run(messages, args) {
     const { message } = messages;
 
+    const esEuro = args[0] == "-euro" ? true : false;
+
     try {
         const response = await axios.get('https://pydolarve.org/api/v2/tipo-cambio');
 
         const data = response.data;
-        const tasaBCV = data.monitors.usd.price;
-        const fecha = data.monitors.usd.last_update;
+        const moneda = esEuro ? data.monitors.eur : data.monitors.usd;
+        const tasaActual = moneda.price; 
+        const tasaVieja = moneda.price_old;
+        const fecha = moneda.last_update;
+        const colorCambio = moneda.color == "green" ? `:green_square:` : `:red_square:`;
+        const razonCambio = moneda.change;
 
-        const respuesta = `**Tasa oficial del BCV: **\`Bs. ${tasaBCV}\` por USD \n- **Ultima fecha**: \`${fecha}\``;
+        const respuesta = `**Tasa oficial del BCV: **\`Bs. ${tasaActual}\` por ${esEuro ? "EUR" : "USD"} \n- **Ultima fecha**: \`${fecha}\` \n- **Cambio** de \`${razonCambio}\` ${colorCambio} \`Bs. ${tasaVieja}\` por ${esEuro ? "EUR" : "USD"} `;
 
         return respuesta;
     } catch (error) {
