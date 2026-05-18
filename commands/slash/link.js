e8b3ed83d26b6d3269b358ece8e3fd57e39ee6b5
jeiden83 <jeiden83@gmail.com>
2026-05-18 01:26:18 -0400

    feat(slash): implement link slash command
    Creates the /link slash command under commands/slash/link.js to link or unlink the Discord member account with an osu! account.

--- commands/slash/link.js
+++ commands/slash/link.js
@@ -1,7 +1,22 @@
-function run(){
-    return "No hace nada, esto es para que no explote por el slash; A implementar";
-}
+const { SlashCommandBuilder } = require("discord.js");
+        const linkChatCommand = require("../chat/osu/link.js");
+        const { addModoOption, parseOsuSlashArgs } = require("../utils/slashUtils.js");
 
-run.description = "Link por medio del slash";
+        const data = new SlashCommandBuilder()
+            .setName("link")
+            .setDescription("Vincula o desvincula tu cuenta de discord con un usuario de osu!")
+            .addStringOption(option =>
+                option.setName("usuario")
+                    .setDescription("Nombre de usuario de osu! a vincular (dejar vacío para desvincular)")
+                    .setRequired(false)
+            )
+            .addStringOption(addModoOption);
 
-module.exports = { run, "description": run.description}
+        async function run(interaction, res) {
+            const { args, messages } = parseOsuSlashArgs(interaction, res);
+            return await linkChatCommand.run(messages, args);
+        }
+
+        run.description = "Vincula o desvincula tu cuenta de discord con un usuario de osu!";
+
+        module.exports = { data, run, description: run.description };
