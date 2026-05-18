a7b2f591c09dbc62b9fa1a814d63371a4b69c858
jeiden83 <jeiden83@gmail.com>
2026-05-18 01:14:17 -0400

    refactor(slash): create slashUtils to centralize shared parameter parser
    Extracts SlashCommandBuilder option helpers and osu slash args parsing to commands/utils/slashUtils.js, keeping slash commands beautifully DRY and modular.

--- commands/slash/osu.js
+++ commands/slash/osu.js
@@ -1,66 +1,16 @@
 const { SlashCommandBuilder } = require("discord.js");
 const osuChatCommand = require("../chat/osu/osu.js");
+const { addUsuarioOption, addModoOption, addServidorOption, parseOsuSlashArgs } = require("../utils/slashUtils.js");
 
 const data = new SlashCommandBuilder()
     .setName("osu")
     .setDescription("Muestra el perfil de un usuario en osu!")
-    .addStringOption(option =>
-        option.setName("usuario")
-            .setDescription("Nombre de usuario de osu! o mención de Discord")
-            .setRequired(false)
-    )
-    .addStringOption(option =>
-        option.setName("modo")
-            .setDescription("Modo de juego de osu!")
-            .setRequired(false)
-            .addChoices(
-                { name: "Standard", value: "std" },
-                { name: "Taiko", value: "taiko" },
-                { name: "Catch the Beat", value: "ctb" },
-                { name: "Mania", value: "mania" }
-            )
-    )
-    .addStringOption(option =>
-        option.setName("servidor")
-            .setDescription("Servidor de osu! (Bancho o Gatari)")
-            .setRequired(false)
-            .addChoices(
-                { name: "Bancho", value: "bancho" },
-                { name: "Gatari", value: "gatari" }
-            )
-    );
+    .addStringOption(addUsuarioOption)
+    .addStringOption(addModoOption)
+    .addStringOption(addServidorOption);
 
 async function run(interaction, res) {
-    const usuario = interaction.options.getString("usuario");
-    const modo = interaction.options.getString("modo");
-    const servidor = interaction.options.getString("servidor");
-
-    // Construir el array de argumentos virtuales
-    const args = [];
-    if (usuario) {
-        // Si el usuario es una mención de Discord (ej: <@395623267530047489>), extraemos solo la ID numérica
-        const mentionMatch = usuario.match(/^<@!?(\d+)>$/);
-        if (mentionMatch) {
-            args.push(mentionMatch[1]);
-        } else {
-            args.push(usuario);
-        }
-    }
-    if (modo) args.push(`-${modo}`);
-    if (servidor) args.push(`-${servidor}`);
-
-    // Construir el objeto de mensajes virtual
-    const messages = {
-        message: {
-            author: interaction.user,
-            member: interaction.member,
-            guild: interaction.guild,
-        },
-        res: res,
-        reply: null
-    };
-
-    // Ejecutar la función run del comando original
+    const { args, messages } = parseOsuSlashArgs(interaction, res);
     return await osuChatCommand.run(messages, args);
 }
 
---
+++ commands/utils/slashUtils.js
@@ -0,0 +1,80 @@
+/**
+ * Utilidades para estructurar y reutilizar código de comandos Slash
+ */
+
+function addUsuarioOption(option) {
+    return option
+        .setName("usuario")
+        .setDescription("Nombre de usuario de osu! o mención de Discord")
+        .setRequired(false);
+}
+
+function addModoOption(option) {
+    return option
+        .setName("modo")
+        .setDescription("Modo de juego de osu!")
+        .setRequired(false)
+        .addChoices(
+            { name: "Standard", value: "std" },
+            { name: "Taiko", value: "taiko" },
+            { name: "Catch the Beat", value: "ctb" },
+            { name: "Mania", value: "mania" }
+        );
+}
+
+function addServidorOption(option) {
+    return option
+        .setName("servidor")
+        .setDescription("Servidor de osu! (Bancho o Gatari)")
+        .setRequired(false)
+        .addChoices(
+            { name: "Bancho", value: "bancho" },
+            { name: "Gatari", value: "gatari" }
+        );
+}
+
+/**
+ * Convierte las opciones estándar de un comando slash en el formato de argumentos y contexto
+ * que esperan los comandos de chat tradicionales.
+ * 
+ * @param {import('discord.js').ChatInputCommandInteraction} interaction 
+ * @param {any} res 
+ * @returns {{ args: string[], messages: any }} Contexto simulado para ejecutar .run()
+ */
+function parseOsuSlashArgs(interaction, res) {
+    const usuario = interaction.options.getString("usuario");
+    const modo = interaction.options.getString("modo");
+    const servidor = interaction.options.getString("servidor");
+
+    const args = [];
+    if (usuario) {
+        // Extraer ID si es una mención de Discord <@ID>
+        const mentionMatch = usuario.match(/^<@!?(\d+)>$/);
+        if (mentionMatch) {
+            args.push(mentionMatch[1]);
+        } else {
+            args.push(usuario);
+        }
+    }
+    if (modo) args.push(`-${modo}`);
+    if (servidor) args.push(`-${servidor}`);
+
+    const messages = {
+        message: {
+            author: interaction.user,
+            member: interaction.member,
+            guild: interaction.guild,
+        },
+        res: res,
+        reply: null
+    };
+
+    return { args, messages };
+}
+
+module.exports = {
+    addUsuarioOption,
+    addModoOption,
+    addServidorOption,
+    parseOsuSlashArgs
+};
